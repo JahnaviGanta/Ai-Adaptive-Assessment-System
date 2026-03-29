55c579d6b0d1dbfbdbd136e39fc09cc10f2d17e7
Pantham Kruthika <panthamkruthika@Panthams-MacBook-Air.local>
2026-03-29 15:33:06 +0530

    updated admin panel

--- backend-drf/assessment/admin.py
+++ backend-drf/assessment/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from . models import Assignment, AssessmentSession
 
-admin.site.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display=['id','topic','title']
+    ordering=['id']
+
+
+
+
+admin.site.register(Assignment, AssignmentAdmin)
 admin.site.register(AssessmentSession)
--- backend-drf/questions/admin.py
+++ backend-drf/questions/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from . models import Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display=['id','assignment','question','answer','difficulty']
+    list_filter =['assignment']
+    search_fields=['assignment__title','difficulty']
+    ordering=['id']
+
+
+admin.site.register(Question, QuestionAdmin)
